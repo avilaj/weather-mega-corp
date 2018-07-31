07e2ae8fdc8bd064aa1a5c12ac8443b5b5cc580d
Jorge Avila <jorge.e.avila@gmail.com>
2018-07-31 17:35:34 -0300

    add cors

--- api/index.js
+++ api/index.js
@@ -6,7 +6,14 @@ const forecast = require('./forecast');
 const ipBasedCity = require('./middlewares/city');
 const errors = require('./middlewares/errors');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const corsOptions = {
+    origin: process.env.SELF_URL,
+    optionsSuccessStatus: 200
+};
+
+router.use(cors(corsOptions))
 router.use(bodyParser.json());
 router.use(errors);
 
